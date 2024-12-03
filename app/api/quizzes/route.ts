436ec5e98d8b5b0597e9f4b4365e95c3eb39d0b4
Amrith R Naik <amrithrnaik@gmail.com>
2024-12-03 23:32:50 +0530

    [feat] implement POST endpoint to create a new quiz with validation

--- app/api/quizzes/route.ts
+++ app/api/quizzes/route.ts
@@ -31,3 +31,41 @@ export const GET = async (req: NextRequest) => {
     );
   }
 };
+
+// Create a new quiz for a subject
+export const POST = async (req: NextRequest) => {
+  const body = await req.json();
+  const { name, subjectId } = body;
+
+  if (!name || !subjectId) {
+    return NextResponse.json(
+      { error: "Name and Subject ID are required" },
+      { status: 400 },
+    );
+  }
+
+  const subject = await prisma.subject.findUnique({
+    where: {
+      id: parseInt(subjectId),
+    },
+  });
+
+  if (!subject) {
+    return NextResponse.json({ error: "Subject not found" }, { status: 404 });
+  }
+
+  try {
+    const quiz = await prisma.quiz.create({
+      data: {
+        name,
+        subjectId: parseInt(subjectId),
+      },
+    });
+    return NextResponse.json(quiz);
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Failed to create quiz" },
+      { status: 500 },
+    );
+  }
+};
