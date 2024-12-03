2c5b60c5e6bef3f49125b3b882f366c891ef0ea1
Amrith R Naik <amrithrnaik@gmail.com>
2024-12-03 18:16:02 +0530

    [feature] Add API routes for subjects with GET and POST methods

---
+++ app/api/subjects/route.ts
@@ -0,0 +1,39 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import prisma from "@/app/lib/prisma";
+import { NextRequest, NextResponse } from "next/server";
+
+// Get all subjects
+export const GET = async () => {
+  try {
+    const subjects = await prisma.subject.findMany();
+    return NextResponse.json(subjects);
+  } catch (error) {
+    return new NextResponse(
+      JSON.stringify({ error: "Failed to fetch subjects" }),
+      {
+        status: 500,
+      }
+    );
+  }
+};
+
+// Create a new subject
+export const POST = async (req: NextRequest) => {
+  const body = await req.json();
+  const { name } = body;
+  try {
+    const subject = await prisma.subject.create({
+      data: {
+        name,
+      },
+    });
+    return NextResponse.json(subject);
+  } catch (error) {
+    return new NextResponse(
+      JSON.stringify({ error: "Failed to create subject" }),
+      {
+        status: 500,
+      }
+    );
+  }
+};
