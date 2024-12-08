43117f2a3c4e7acb10c944db4f5198c7f2398411
Amrith R Naik <amrithrnaik@gmail.com>
2024-12-08 11:26:53 +0530

    [feat] add shadcn Button component

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <h1 className="text-7xl font-black">Hello. This is fun right?</h1>
+        <h1 className="text-7xl font-black">
+          Wanna practice those MCQ subjects?
+        </h1>
+        <Button>Yes</Button>
       </main>
     </div>
   );
